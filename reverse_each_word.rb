4d9e925fb59280f25e3d0cba0238664ea05d0ad1
Justin Winger <justinwinger4@gmail.com>
2020-03-25 17:47:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,5 @@ reverse_each_word(sentence)
   sentence = sentence.split(" ")
   new_array = []
   new_array = words_array.collect { |word| word.reverse }
-  list.to_a.collec 
-  return a
+  new_array.join(" ")
 end
